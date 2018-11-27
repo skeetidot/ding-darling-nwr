58ccf0b37814c3f926a5bf80d3bb7c1caf59d569
Lauren Winkler <lewinkler2@wisc.edu>
2018-11-27 03:39:22 -0500

    Revert "Update path to PHP proxy"
    This reverts commit 379cfecb90e790e8d2a431e1f884f599358f278f.

--- php/callProxy.php
+++ php/callProxy.php
@@ -3,7 +3,7 @@
 // This file calls the proxy, sets the query we pass to it equal to a variable, then runs a function named goProxy using the query as an argument.
 
 // Call the proxy
-include 'php/cartoDBProxy.php';
+include 'C:\xampp\htdocs\ding-darling-nwr\php\cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 
 // Set the query we pass to the proxy to the $queryURL variable
